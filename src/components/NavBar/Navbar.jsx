ff8795079bcd0f74932bd7e8ec4e8e4b98e405f6
Dimitri Finger <dimitri.finger@outlook.com>
2022-06-19 23:09:05 -0300

    wip

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,11 +1,38 @@
-import { NavDiv, Logo } from "./styled"
+import React, { useContext, useState } from 'react';
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink,
+    Logo
+} from './styled';
+const NavBar = () => {
 
-const Navbar = () => {
+
+
     return (
-        <NavDiv>
-            <Logo />
-        </NavDiv>
+        <>
+            <Nav>
+
+                <Logo>dimitri.finger</Logo>
+
+                <Bars />
+                <NavMenu >
+                    <NavLink to='/about' >
+                        About
+                    </NavLink>
+                    <NavLink to='/contact' >
+                        Contact Us
+                    </NavLink>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to='/login'>Sign In</NavBtnLink>
+                </NavBtn>
+            </Nav>
+        </>
     )
 }
 
-export default Navbar
+export default NavBar
