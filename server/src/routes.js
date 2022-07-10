dba45ee9ed9df9596cfac316e756e4d286852418
Dimitri Finger <dimitri.finger@outlook.com>
2022-07-09 22:05:42 -0300

    Series routes added

--- server/src/routes.js
+++ server/src/routes.js
@@ -9,5 +9,17 @@ routes.get('/users/:id', UserController.getUser);
 routes.put('/users/:id', UserController.updateUser)
 routes.delete('/users/:id', UserController.deleteUser);
 
+routes.get('/movies', UserController.getAllUsers);
+routes.post('/movies', UserController.createUser);
+routes.get('/movies/:id', UserController.getUser);
+routes.put('/movies/:id', UserController.updateUser)
+routes.delete('/movies/:id', UserController.deleteUser);
+
+routes.get('/series', UserController.getAllUsers);
+routes.post('/series', UserController.createUser);
+routes.get('/series/:id', UserController.getUser);
+routes.put('/series/:id', UserController.updateUser)
+routes.delete('/series/:id', UserController.deleteUser);
+
 
 export default routes;
